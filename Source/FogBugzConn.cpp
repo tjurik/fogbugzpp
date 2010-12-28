b83f5c7c2c082cdb236e06ea4d908b1cddf25237
kevinchkin <evilkamikaze@gmail.com>
2010-12-27 19:54:46 -0800

    Adding author name

--- Source/FogBugzConn.cpp
+++ Source/FogBugzConn.cpp
@@ -1,6 +1,8 @@
 /*
  Copyright (c) 
  All rights reserved.
+
+ Author: kevinchkin
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  
